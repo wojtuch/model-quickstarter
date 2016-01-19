b2ab84c218caa53408138b397632a5bf9ae3fc62
Jo Daiber <jodaiber@gmx.de>
2016-01-19 20:28:57 +0100

    Removed duplicate French

--- run.sh
+++ run.sh
@@ -14,7 +14,6 @@ echo "Indexing..."
 ./index_db.sh wdir pt_BR pt/stopwords.list Portuguese models/pt
 ./index_db.sh wdir ja_JP ja/stopwords.list None models/ja
 ./index_db.sh wdir es_ES es/stopwords.list Spanish models/es
-./index_db.sh wdir fr_FR fr/stopwords.list French models/fr
 ./index_db.sh wdir cs_CZ cs/stopwords.list None models/cs
 ./index_db.sh wdir it_IT it/stopwords.list Italian models/it
 ./index_db.sh wdir hu_HU hu/stopwords.list Hungarian models/hu
