c374a0625daa2b5b77ae2edd0a49e8dfa230234d
Jo Daiber <jodaiber@gmx.de>
2016-01-28 23:19:45 +0100

    getopts options must be first

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Indexing..."
-./index_db.sh wdir en_US en/stopwords.list English models/en -b en/ignore.list
-./index_db.sh wdir de_DE de/stopwords.list German  models/de -b de/ignore.list
-./index_db.sh wdir nl_NL nl/stopwords.list Dutch   models/nl -b nl/ignore.list
-./index_db.sh wdir sv_SE sv/stopwords.list Swedish models/sv -b sv/ignore.list
-./index_db.sh wdir pt_BR pt/stopwords.list Portuguese models/pt -b pt/ignore.list
-./index_db.sh wdir fr_FR fr/stopwords.list French models/fr -b fr/ignore.list
-./index_db.sh wdir es_ES es/stopwords.list Spanish models/es -b es/ignore.list
-./index_db.sh wdir tr_TR tr/stopwords.list Turkish models/tr -b tr/ignore.list
+./index_db.sh -b en/ignore.list wdir en_US en/stopwords.list English models/en
+./index_db.sh -b de/ignore.list wdir de_DE de/stopwords.list German  models/de
+./index_db.sh -b nl/ignore.list wdir nl_NL nl/stopwords.list Dutch   models/nl
+./index_db.sh -b sv/ignore.list wdir sv_SE sv/stopwords.list Swedish models/sv
+./index_db.sh -b pt/ignore.list wdir pt_BR pt/stopwords.list Portuguese models/pt
+./index_db.sh -b fr/ignore.list wdir fr_FR fr/stopwords.list French models/fr
+./index_db.sh -b es/ignore.list wdir es_ES es/stopwords.list Spanish models/es
+./index_db.sh -b tr/ignore.list wdir tr_TR tr/stopwords.list Turkish models/tr
 ./index_db.sh wdir it_IT it/stopwords.list Italian models/it
 ./index_db.sh wdir da_DK da/stopwords.list Danish models/da
 ./index_db.sh wdir ja_JP ja/stopwords.list None models/ja
